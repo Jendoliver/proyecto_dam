c24455ecc002f56b8310a1b9bfaaccca1d2f4193
Jendoliver <jandol1996@hotmail.com>
2017-03-08 15:22:09 +0000

    Close #1

--- src/front_end/footer.php
+++ src/front_end/footer.php
@@ -58,7 +58,7 @@
             </div>
         </div><br>
         <div class="row">
-            <p><center>¿Tienes alguna duda? <a href="#">¡Contáctanos!</a> <p class="footertext">Copyright 2017 ©</p></center></p>
+            <p><center>¿Tienes alguna duda? <a href="#">¡Contáctanos man!</a> <p class="footertext">Copyright 2017 ©</p></center></p>
         </div>
     </div>
 </div>

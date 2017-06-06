410ca637d0b0e13e0c50cd1b59a112250e1be0d7
guim1111 <guim111@hotmail.com>
2017-06-06 23:20:59 +0000

    solutionated errror fatal

--- src/back_end/libs/mail_lib.php
+++ src/back_end/libs/mail_lib.php
@@ -22,20 +22,32 @@ $headers = array ('From' => $from,'To' => $to,'Subject' => $subject);
 $smtp = Mail::factory('smtp',array ('host' => $host,'auth' => true,'username' => $username, 'password' => $password));
 $mail = $smtp->send($to, $headers, $body);
 
-//Una vez aquí habremos enviado el mensaje mediante el formulario
-//El siguiente código muestra en pantalla un mensaje indicando que el mensaje ha sido enviado y a que cuenta ES OPCIONAL desde Acens lo incluimos para verificar que el formulario de prueba esta funcionando.
-if (PEAR::isError($mail)||$to="error") 
-{
-          echo("" . $mail->getMessage() . "");
-} 
-else 
-{
-          updatePass($use, $userpass);
-          echo "Mensaje enviado desde lalechemusic a ". $to ;
-          header('Location: /front_end/index.php');
+updatePass($use, $userpass);
+echo "Mensaje enviado desde lalechemusic a ". $to ;
+header('Location: http://lalechemusic.com/front_end/index.php');
 }
-}
+if(isset($_POST["sendauto"]))
+{
 
+error_reporting( E_ALL & ~( E_NOTICE | E_STRICT | E_DEPRECATED ) ); //Aquí se genera un control de errores "NO BORRAR NI SUSTITUIR"
+require_once "Mail.php"; //Aquí se llama a la función mail "NO BORRAR NI SUSTITUIR"
+
+$to = 'milkman@lalechemusic.com';//Aquí definimos quien recibirá el formulario
+$from = 'milkman@lalechemusic.com'; //Aquí definimos que cuenta mandará el correo, generalmente perteneciente al mismo dominio
+$host = 'smtp.lalechemusic.com'; //Aquí definimos cual es el servidor de correo saliente desde el que se enviaran los correos
+$username = 'milkman@lalechemusic.com'; //Aqui se define el usuario de la cuenta de correo
+$password = 'a53318214M!'; //Aquí se define la contraseña de la cuenta de correo que enviará el mensaje
+$subject = $_POST["asunto"]; //Aquí se define el asunto del correo
+$body = $_POST["contenido"]; //Aquí se define el cuerpo de correo
+
+//A partir de aquí empleamos la función mail para enviar el formulario 
+$headers = array ('From' => $from,'To' => $to,'Subject' => $subject);
+$smtp = Mail::factory('smtp',array ('host' => $host,'auth' => true,'username' => $username, 'password' => $password));
+$mail = $smtp->send($to, $headers, $body);
+
+echo "Mensaje enviado desde lalechemusic a ". $to ;
+header('Location: http://lalechemusic.com/front_end/index.php');
+}
 function selectemail($username)
 {
     $con = conectar($GLOBALS['db']);
